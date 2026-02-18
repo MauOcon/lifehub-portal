27485decdadea933a4cac17cda0ea48ae283e8bb
mauocool <mauoconolivares@gmail.com>
2026-02-18 17:23:55 -0600

    Adds base layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('lifehub-portal');
+  openSubmenus = new Set<string>();
+
+  toggleSubmenu(submenuId: string) {
+    if (this.openSubmenus.has(submenuId)) {
+      this.openSubmenus.delete(submenuId);
+    } else {
+      this.openSubmenus.add(submenuId);
+    }
+    console.log(this.openSubmenus);
+  }
+
+  isSubmenuOpen(submenuId: string): boolean {
+    return this.openSubmenus.has(submenuId);
+  }
 }
